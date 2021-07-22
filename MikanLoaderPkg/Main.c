6c8d02c8de67791feadac6cb62bad0153f2b3b8d
diceesugar <workshift56@gmail.com>
2021-07-22 12:59:14 +0900

    fix type...

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -26,7 +26,7 @@ EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
     &map->map_size,
     (EFI_MEMORY_DESCRIPTOR*)map->buffer,
     &map->map_key,
-    &map->descriptor_size
+    &map->descriptor_size,
     &map->descriptor_version);
 }
 
